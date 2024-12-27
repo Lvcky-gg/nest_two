2a54532450fcd131d34d1d2107aa3055af3f5f0f
lvcky-gg <matadorcrab@gmail.com>
2024-12-27 11:49:05 -0500

    chore: init

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,9 +1,21 @@
 import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
-  async findOne(id: string) {}
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
 
-  async findAll() {}
+  async findAll() {
+    return JSON.parse(await readFile('messages.json', 'utf-8'));
+  }
 
-  async create(message: string) {}
+  async create(message: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, message };
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
